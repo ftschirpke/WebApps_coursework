32530aeac5aad58a78096bcfffc9751046d2ee21
Friedrich Tschirpke <tschirpf@student.hu-berlin.de>
2022-02-25 20:12:45 +0100

    changed seeding emails

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -18,14 +18,14 @@ class UsersTableSeeder extends Seeder
     {
         $test1 = new User();
         $test1->name = "Test User 1";
-        $test1->email = "2130499@swansea.ac.uk";
+        $test1->email = "funny@email.com";
         $test1->email_verified_at = now();
         $test1->password = "test1";
         $test1->display_name = "funny name";
         $test1->save();
         $test2 = new User();
         $test2->name = "Test User 2";
-        $test2->email = "tschirpf@hu-berlin.de";
+        $test2->email = "hilarious@email.com";
         $test2->email_verified_at = now();
         $test2->password = "test2";
         $test2->display_name = "hilarious name";
