67386ea68e6bc258a0deada8e7073e91deffef0b
Friedrich Tschirpke <tschirpf@student.hu-berlin.de>
2022-03-04 16:48:35 +0100

    nicer views for settings, account and home

---
+++ resources/views/components/settings.blade.php
@@ -0,0 +1,4 @@
+<x-navbar active="settings"></x-navbar>
+<x-layout>
+    <h1>Settings</h1>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
+use App\Models\User;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| contains the 'web' middleware group. Now create something great!
 |
 */
 
@@ -19,22 +21,22 @@ Route::get('/', function () {
 });
 
 Route::get('/home', function() {
-    return "homepage XD";
-})->name("home");
+    return view('components.home');
+})->name('home');
 
 Route::get('/settings', function () {
-    return "settings";
-})->name("settings");
+    return view('components.settings');
+})->name('settings');
 
 Route::get('/account', function () {
-    return "account";
-})->name("account");
+    return view('components.account', ['account'=>User::find(1)->account]);
+})->name('account');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts', [PostController::class, 'index'])->name('all_posts');
 
 Route::get('/posts/{id}', [PostController::class, 'show']);
 
