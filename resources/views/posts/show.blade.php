152bd77a3526c92a188cc43380233d65c3467561
Friedrich Tschirpke <tschirpf@student.hu-berlin.de>
2022-03-23 15:43:11 +0100

    small change to pagination of comments

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,9 +7,18 @@
 
     <x-post :post='$post'></x-post>
     
-    <!-- <h3>Comments</h3> -->
-    <div class="d-flex p-4 justify-content-center">
-        {{ $post->comments()->paginate(10)->links() }}
+    <div class="row">
+        <div class="col">
+            <div class="d-flex p-4">
+                <h3>Comments ({{ $post->comments()->count() }})</h3>
+            </div>
+        </div>
+        <div class="col">
+            <div class="d-flex p-4 justify-content-center">
+                {{ $post->comments()->paginate(10)->links() }}
+            </div>
+        </div>
+        <div class="col"></div>
     </div>
     @foreach ($post->comments()->paginate(10) as $comment)
     <div class="container-fluid p-2 bg-secondary">
@@ -17,14 +26,14 @@
             <div class="col col-1"></div>
                 <div class="col col-10">
                     <div class="row">
-                        <x-comment :comment='$comment'></x-comment>
+                        <x-comment :comment="$comment"></x-comment>
                     </div>
                 </div>
             <div class="col col-1"></div>
         </div>
     </div>
     @endforeach<div class="d-flex p-4 justify-content-center">
-        {{ $post->comments()->paginate(10) }}
+    {{ $post->comments()->paginate(10)->links() }}
     </div>
     
 
