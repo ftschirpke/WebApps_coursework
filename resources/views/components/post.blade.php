e2c3d3963e6ea58256f12ff87368404892f4b895
Friedrich Tschirpke <tschirpf@student.hu-berlin.de>
2022-03-23 16:41:04 +0100

    revert changes lost through merge mistake

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -3,7 +3,7 @@
         <div class="container-fluid p-4">
             <h1>{{ $post->title ?? 'Post without title' }}</h1>
 
-            <h5>by <a class="text-warning" href="{{ route('account', ['id' => $post->user->id]) }}">
+            <h5>{{ $post->public ? "Public Post" : "Private Post" }} by <a class="text-warning" href="{{ route('account', ['id' => $post->user->id]) }}">
                 {{ $post->user->account->display_name }}
             </a></h5>
                 
@@ -11,7 +11,9 @@
                 @if (!is_null($post->image))
                     <img src="{{ asset($post->image) }}" class="img-fluid pt-1 pe-3 pb-3 float-sm-start"/>
                 @endif
-                <p class="pull-left">{{ $post->message }}</p>
+                <p class="pull-left">{!! nl2br(e($post->message)) !!}</p>
+                <!-- this converts line breaks to br-tags, such that
+                    the text is still nicely formatted -->
             </div>
         </div>
     </div>
