3968fc4cbf660bb22d7aa7d825bf956cc67a7e0e
Friedrich Tschirpke <tschirpf@student.hu-berlin.de>
2022-03-25 10:26:51 +0100

    Posts display creation and update time now

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -2,12 +2,22 @@
     <div class="col-md-auto">
         <div class="container-fluid p-4">
             <h1>{{ $post->title ?? 'Post without title' }}</h1>
-
-            <h5>{{ $post->public ? "Public Post" : "Private Post" }} by 
-                <a class="text-warning" href="{{ route('accounts.show', ['account' => $post->user->account]) }}">
-                    {{ $post->user->account->display_name }}
-                </a>
-            </h5>
+            
+            <div class="row mb-2">
+                <div class="col-5">
+                    <h5>{{ $post->public ? "Public Post" : "Private Post" }} by 
+                        <a class="text-warning" href="{{ route('accounts.show', ['account' => $post->user->account]) }}">
+                            {{ $post->user->account->display_name }}
+                        </a>
+                    </h5>
+                </div>
+                <div class="col-7 text-end">
+                    Created on {{ $post->created_at; }}
+                    @if ( $post->updated_at != $post->created_at )
+                    and Updated on {{ $post->updated_at; }}
+                    @endif
+                </div>
+            </div>
                 
             <div class="clearfix">
                 @if (!is_null($post->image))
