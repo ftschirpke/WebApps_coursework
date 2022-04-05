7ddcde61b976684e5b60e470f2eb81501d82b190
Friedrich Tschirpke <tschirpf@student.hu-berlin.de>
2022-04-05 15:21:56 +0100

    added pagination and themed the pagination links

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,13 +5,21 @@
             <div class="container-fluid p-4">
                 <h1>List of all posts</h1>
                 <a href="{{ route('posts.create') }}">Create Post</a>
-                @foreach ($posts as $post)
-                    <li>
-                        <a class="text-warning" href="{{ route('posts.show', $post) }}">
-                            {{ $post->title }}
-                        </a>
-                    </li>
-                @endforeach
+                <div class="d-flex p-1 justify-content-center">
+                    {{ $posts->links() }}
+                </div>
+                <ul>
+                    @foreach ($posts as $post)
+                        <li>
+                            <a class="text-warning" href="{{ route('posts.show', $post) }}">
+                                {{ $post->title }}
+                            </a>
+                        </li>
+                    @endforeach
+                </ul>
+                <div class="d-flex p-1 justify-content-center">
+                    {{ $posts->links() }}
+                </div>
             </div>
         </div>
     </span>
