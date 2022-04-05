60d3c772d7540db2e2fc9e077fe93566376acdb7
Friedrich Tschirpke <tschirpf@student.hu-berlin.de>
2022-04-05 16:10:58 +0100

    asking for confirmation for post deletion

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -2,11 +2,13 @@
     <div class="col-md-auto">
         <div class="container-fluid p-4">
             <h1>{{ $post->title ?? 'Post without title' }}</h1>
-            <form method="POST" action="{{ route('posts.destroy', $post) }}">
-                @csrf
-                @method('DELETE')
-                <button class="btn btn-warning" type="submit">Delete this Post</button>
-            </form>
+            <x-delete-button type="Post">
+                <form method="POST" action="{{ route('posts.destroy', $post) }}">
+                    @csrf
+                    @method('DELETE')
+                    <button class="btn btn-warning" type="submit">Yes</button>
+                </form>
+            </x-delete-button>
             <div class="row mb-2">
                 <div class="col-5">
                     <h5>{{ $post->public ? "Public Post" : "Private Post" }} by 
